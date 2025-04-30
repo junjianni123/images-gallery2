a1b2c5357b6076bac2f78d3dd812568d8945a86c
Junjian Ni <junjian.ni@syngenta.com>
2025-04-30 15:00:46 -0400

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
-function App() {
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Search from './components/Search';
+
+
+
+const App = () => {
+  const [word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+
   return (
-    <div className="App">
-      <h1>Image Gallery</h1>
+    <div>
+      <Header title="Images Gallery"/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
